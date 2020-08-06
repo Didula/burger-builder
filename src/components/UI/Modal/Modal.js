8e098ba935ef1250b0f6240a54b3831dc832dd07
devo.dce@gmail.com <d!dula1991>
2020-08-06 16:50:22 +0530

    Performance optimization

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import classes from "./Modal.module.css"
 import Auxiliary from '../../../hoc/Auxilary'
 import BackDrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Auxiliary>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal}
-             style={{
-                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1' : '0'
-             }}>
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Modal updated')
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Modal;

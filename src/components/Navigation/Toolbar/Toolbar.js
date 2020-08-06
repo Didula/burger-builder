5db8b508a427ecf7d9f46936765c6a656f0193ed
devo.dce@gmail.com <d!dula1991>
2020-08-06 15:24:29 +0530

    Adding side drawer controls

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from "react";
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
+        <SideDrawerToggle menuClicked={props.opened}/>
         <div className={classes.Logo}><Logo/></div>
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
